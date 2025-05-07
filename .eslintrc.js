eaf8854ad172263b7c4b45911d06a76adf996259
tomas6k <tomas.cikanauskas@proton.me>
2025-05-07 16:29:11 +0200

    Fix: configuration Netlify et ESLint pour le déploiement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'error'
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
   }
 }; 
