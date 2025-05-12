c032ba0b2a99e73d70320254ed63d0eed339c500
tomas6k <tomas.cikanauskas@proton.me>
2025-05-12 09:27:58 +0200

    feat: ajout des favicons et intégration dans le layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,14 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`min-h-screen bg-[url('/background.png')] bg-repeat ${inter.variable}`}>
         <Providers>
